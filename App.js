c74273626e5ddcebbf76d340b50b3744f3008c59
aswin-codes <codicon.co@gmail.com>
2023-04-05 12:34:41 +0530

    Commit before Integration React Navigation

--- App.js
+++ App.js
@@ -5,9 +5,9 @@
  * @format
  */
 
-import { NativeBaseProvider, Box, Text, VStack, Center, HStack, Switch, Heading, ScrollView,  Flex, Actionsheet, useDisclose, Input, Button, Image, FormControl, Alert } from "native-base";
+import { NativeBaseProvider, Box, Text, VStack, Center, HStack, Switch, Heading, ScrollView, Flex, Actionsheet, useDisclose, Input, Button, Image, FormControl, Alert } from "native-base";
 import Icon from 'react-native-vector-icons/AntDesign';
-import { useState, useEffect}from 'react';
+import { useState, useEffect } from 'react';
 import {
   SafeAreaView,
   StatusBar,
@@ -27,15 +27,16 @@ import {
 } from 'react-native/Libraries/NewAppScreen';
 import Item from './components/Item';
 import { DateTimePickerAndroid } from '@react-native-community/datetimepicker';
-import {launchImageLibrary} from 'react-native-image-picker';
+import { launchImageLibrary } from 'react-native-image-picker';
 import RNFetchBlob from 'rn-fetch-blob';
 import "react-native-get-random-values";
 import { v4 as uuidv4 } from 'uuid';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import PushNotification from "react-native-push-notification"
+const UUID = require('uuid-int');
 
 
-function App(){
+function App() {
 
   const [image, setImage] = useState(null);
   const [title, setTitle] = useState('');
@@ -62,7 +63,7 @@ function App(){
       }
       else {
         console.log(stored.assets[0].key)
-      
+
         setData(stored)
         setIsLoading(false);
       }
@@ -71,16 +72,22 @@ function App(){
     }
   }
 
-  const setNotification = (key,thedate,title) => {
+  const setNotification = (key, thedate, title) => {
+
+    console.log(thedate.toLocaleString());
+    console.log(key, title);
+    console.log(typeof (thedate));
 
     PushNotification.localNotificationSchedule({
       id: key,
       channelId: "wisher",
       title: title,
       message: "Hey mate, you got wish remainder!",
-      date: new Date(Date.now() + 10*1000),
+      date: thedate,
       allowWhileIdle: true,
-  });
+      repeatType: "year"
+    });
+    console.log("Scheduled Notification");
     // Notifications.scheduleNotificationAsync({
     //   identifier: key,
     //   content: {
@@ -97,7 +104,7 @@ function App(){
     // })
 
 
-    console.log("Scheduled Notification");
+
   }
 
   const handleChannel = () => {
@@ -120,7 +127,7 @@ function App(){
   //Styles
   const backgroundStyle = {
     backgroundColor: isDarkMode ? '#010001' : '#EEEEEE',
-    flex : 1,
+    flex: 1,
     paddingTop: 5
   };
 
@@ -130,50 +137,50 @@ function App(){
     height: 45,
     paddingTop: 30,
     paddingLeft: 10,
-    color: isDarkMode ? '#FFFFFF':'#010001'
+    color: isDarkMode ? '#FFFFFF' : '#010001'
   }
 
   const getDate = (jsDate) => {
-    if (typeof(jsDate) == 'string'){
+    if (typeof (jsDate) == 'string') {
       jsDate = new Date(jsDate)
     }
     var date = parseInt(jsDate.getDate());
-    if (date < 10){
-      date = "0"+date.toString();
+    if (date < 10) {
+      date = "0" + date.toString();
     }
     else {
       date = date.toString();
     }
-    var month =  parseInt(jsDate.getMonth());
-    if (month < 9){
-      month = "0" + (month+1).toString();
+    var month = parseInt(jsDate.getMonth());
+    if (month < 9) {
+      month = "0" + (month + 1).toString();
     }
     else {
-      month = (month+1).toString();
+      month = (month + 1).toString();
     }
     var year = jsDate.getFullYear().toString();
-    return date+" - "+month+" - "+year
+    return date + " - " + month + " - " + year
   }
 
   const getTime = (jsDate) => {
-    if (typeof(jsDate) == 'string'){
+    if (typeof (jsDate) == 'string') {
       jsDate = new Date(jsDate)
     }
     var minute = parseInt(jsDate.getMinutes());
-    if (minute < 10){
-      minute = "0"+minute.toString();
+    if (minute < 10) {
+      minute = "0" + minute.toString();
     }
     else {
       minute = minute.toString();
     }
     var hour = parseInt(jsDate.getHours())
-    if (hour < 10){
-      hour = "0"+hour.toString();
+    if (hour < 10) {
+      hour = "0" + hour.toString();
     }
     else {
       hour = hour.toString();
     }
-    return hour+" : "+minute
+    return hour + " : " + minute
   }
 
   const {
@@ -186,13 +193,13 @@ function App(){
     const StoreData = async () => {
       try {
         const jsonData = JSON.stringify(data);
-        await AsyncStorage.setItem("data",jsonData);
+        await AsyncStorage.setItem("data", jsonData);
       } catch (e) {
         console.log(e);
       }
     }
     StoreData();
-    data.assets.map((i)=>{
+    data.assets.map((i) => {
       console.log(i.date);
     })
     //console.log(data);
@@ -200,15 +207,25 @@ function App(){
 
   const deleteNotifications = () => {
     //Notifications.cancelAllScheduledNotificationsAsync()
+    PushNotification.cancelAllLocalNotifications();
+    console.log("Cancelled All Notifications");
+  }
+
+  const getAll = () => {
+    PushNotification.getScheduledLocalNotifications((e)=>{
+      console.log("Notifications : ",e);
+    });
+  
   }
 
   const deleteData = () => {
-    setData({assets:[]});
+    setData({ assets: [] });
   }
 
   const handleTitle = text => setTitle(text)
 
   const onChange = (event, selectedDate) => {
+    selectedDate.setSeconds(0);
     const currentDate = selectedDate;
     setDate(currentDate);
   };
@@ -233,15 +250,17 @@ function App(){
   const getKey = () => {
     var listOfData = data.assets;
     var res = true;
-    var key = uuidv4();
+    const id = 0;
+    const generator = UUID(id);
+    var key = parseInt(generator.uuid().toString().slice(-9));
     console.log("Key Generated : ", key);
-    for (let i=0; i < listOfData.length; i++){
-      if (key == listOfData[i].key){
+    for (let i = 0; i < listOfData.length; i++) {
+      if (key == listOfData[i].key) {
         res = false;
         break;
       }
     }
-    if (res){
+    if (res) {
       return key
     }
     else {
@@ -271,7 +290,7 @@ function App(){
         mediaType: 'photo',
         selectionLimit: 1
       },
-      async (response) =>  {
+      async (response) => {
         if (response.didCancel) {
           console.log('User cancelled image picker');
         } else if (response.error) {
@@ -294,7 +313,7 @@ function App(){
           //console.log(imagePath);
           //await RNFetchBlob.fs.writeFile(imagePath, imageBase, 'base64')
           //.then((success) => {
-            //console.log("File Written");
+          //console.log("File Written");
           //})
           //.catch((e) => console.log(e))
           //setImageURI("file:///"+imagePath);
@@ -334,11 +353,12 @@ function App(){
           date: date,
           image: base64,
           status: true,
-          key:key,
+          key: key,
         })
         return a
       })
-      setNotification(key,date,title);
+      console.log(date);
+      setNotification(key, date, title);
       handleCancel();
 
     }
@@ -346,17 +366,22 @@ function App(){
 
   return (
     <NativeBaseProvider>
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <Box>
-      <Text style={titleStyle}>Wisher</Text>
-      </Box>
-
-      <ScrollView>
-      {
+      <SafeAreaView style={backgroundStyle}>
+        <StatusBar
+          barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+          backgroundColor={backgroundStyle.backgroundColor}
+        />
+        <Box>
+          <Text style={titleStyle}>Wisher</Text>
+        </Box>
+        {/* <VStack space={3}>
+          <Button onPress={() => setNotification(2147483647, new Date(Date.now() + 10 * 1000), "Hi")}>Press</Button>
+          <Button colorScheme="error" onPress={deleteNotifications}>Delete All Notifications</Button>
+          <Button colorScheme="error" onPress={deleteData}>Delete All Data</Button>
+          <Button colorScheme="error" onPress={getAll}>Get All Notifications</Button>
+        </VStack> */}
+        <ScrollView>
+          {
             (data.assets.length == 0)
               ?
               <VStack height={0.7 * Dimensions.get('window').height} paddingTop='15px' paddingBottom="20px" justifyContent="center" alignItems="center">
@@ -365,71 +390,70 @@ function App(){
               :
               <VStack paddingTop='15px' paddingBottom="20px" space={4} alignItems='center'>
                 {
-                  data.assets.map((element) => <Item key={element.key} title={element.title}  status={element.status} time={getTime(element.date)} date={getDate(element.date)} />)
+                  data.assets.map((element) => <Item key={element.key} title={element.title} status={element.status} time={getTime(element.date)} date={getDate(element.date)} />)
                 }
-                <Button colorScheme="error" onPress={deleteNotifications}>Delete All Notifications</Button>
-                <Button colorScheme="error" onPress={deleteData}>Delete All Data</Button>
+
               </VStack>
           }
 
-      </ScrollView>
-      <Flex alignItems="center" h="8%" w="100%" bgColor={isDarkMode ? '#010001' : '#EEEEEE'} >
-        <TouchableOpacity onPress={onOpen}><Icon name="pluscircle" size={60} color='#0487D9' style={{bottom: 20}} /></TouchableOpacity>
-      </Flex>
+        </ScrollView>
+        <Flex alignItems="center" h="8%" w="100%" bgColor={isDarkMode ? '#010001' : '#EEEEEE'} >
+          <TouchableOpacity onPress={onOpen}><Icon name="pluscircle" size={60} color='#0487D9' style={{ bottom: 20 }} /></TouchableOpacity>
+        </Flex>
 
-      <Actionsheet  isOpen={isOpen} onClose={onClose}>
-        <Actionsheet.Content backgroundColor={isDarkMode ? '#242525' : '#EEEEEE'} height="600px">
-          <Box w="100%" h={60} px={4} justifyContent="center">
-            <Text fontSize="25" color={isDarkMode ? "#EEEEEE" : "gray.600"} >
-              Add a Wish !
-            </Text>
-          </Box>
-          <Actionsheet.Item backgroundColor={isDarkMode ? '#242525' : '#EEEEEE'} >
-            <Input value={title} onChangeText={handleTitle} fontSize={20} fontFamily={'Roboto-Regular'} placeholder='Add a title' width={0.85 * Dimensions.get('window').width} />
-          </Actionsheet.Item>
-          <Actionsheet.Item backgroundColor={isDarkMode ? '#242525' : '#EEEEEE'}>
-            <HStack width={0.85 * Dimensions.get('window').width} justifyContent="space-between" alignItems="center">
-              <FormControl width={0.39 * Dimensions.get('window').width}>
-                <Button fontSize="25px" leftIcon={<Icon color="#0487D9" size={30} style={{marginRight:20, marginLeft: 10}} name='calendar' />} size='md' variant="outline" onPress={showDatepicker} ><Text marginRight="10px" color="#0487D9">{getDate(date)}</Text></Button>
-                <FormControl.HelperText>Select Date for wish</FormControl.HelperText>
-              </FormControl>
-              <FormControl width={0.40 * Dimensions.get('window').width}>
-                <Button paddingRight="10px" leftIcon={<Icon color="#0487D9" size={30} name='clockcircleo' style={{marginRight: 20}} />} size='md' variant="outline" onPress={showTimepicker}  ><Text marginRight="20px" color="#0487D9">{getTime(date)}</Text></Button>
-                <FormControl.HelperText>Enter time for the notification</FormControl.HelperText>
-              </FormControl>
-            </HStack>
-          </Actionsheet.Item>
-          <Actionsheet.Item backgroundColor={isDarkMode ? '#242525' : '#EEEEEE'}>
-            <HStack width={0.85 * Dimensions.get('window').width} justifyContent="space-between">
-              <Button backgroundColor="#0487D9" height="10" onPress={pickImage}> Select Image</Button>
-              {image && <Image resizeMode='contain' alt='Image' source={{ uri: image }} style={{ width: 108, height: 192 }} />}
-            </HStack>
-          </Actionsheet.Item>
-
-          <Actionsheet.Item backgroundColor={isDarkMode ? '#242525' : '#EEEEEE'}>
-            <HStack width={0.85 * Dimensions.get('window').width} justifyContent="space-between"  >
-              <Button onPress={handleCancel} backgroundColor="danger.600" w="20">Cancel</Button>
-              <Button onPress={handleAdd} backgroundColor="#0487D9" w="20"><Text fontFamily="Roboto" fontSize="16px" color="white" >Add</Text></Button>
-            </HStack>
-          </Actionsheet.Item>
-          {
-            message && <Actionsheet.Item>
-              <HStack width={0.85 * Dimensions.get('window').width} justifyContent="center">
-                <Alert status={stat} colorScheme={stat}>
-                  <HStack space={2} justifyContent="space-between" >
-                    <Alert.Icon />
-                    <Text>{message}</Text>
-                  </HStack>
-                </Alert>
+        <Actionsheet isOpen={isOpen} onClose={onClose}>
+          <Actionsheet.Content backgroundColor={isDarkMode ? '#242525' : '#EEEEEE'} height="600px">
+            <Box w="100%" h={60} px={4} justifyContent="center">
+              <Text fontSize="25" color={isDarkMode ? "#EEEEEE" : "gray.600"} >
+                Add a Wish !
+              </Text>
+            </Box>
+            <Actionsheet.Item backgroundColor={isDarkMode ? '#242525' : '#EEEEEE'} >
+              <Input value={title} color={isDarkMode ? "white" : "black"} onChangeText={handleTitle} fontSize={20} fontFamily={'Roboto-Regular'} placeholder='Add a title' width={0.85 * Dimensions.get('window').width} />
+            </Actionsheet.Item>
+            <Actionsheet.Item backgroundColor={isDarkMode ? '#242525' : '#EEEEEE'}>
+              <HStack width={0.85 * Dimensions.get('window').width} justifyContent="space-between" alignItems="center">
+                <FormControl width={0.39 * Dimensions.get('window').width}>
+                  <Button fontSize="25px" leftIcon={<Icon color="#0487D9" size={30} style={{ marginRight: 20, marginLeft: 10 }} name='calendar' />} size='md' variant="outline" onPress={showDatepicker} ><Text marginRight="10px" color="#0487D9">{getDate(date)}</Text></Button>
+                  <FormControl.HelperText>Select Date for wish</FormControl.HelperText>
+                </FormControl>
+                <FormControl width={0.40 * Dimensions.get('window').width}>
+                  <Button paddingRight="10px" leftIcon={<Icon color="#0487D9" size={30} name='clockcircleo' style={{ marginRight: 20 }} />} size='md' variant="outline" onPress={showTimepicker}  ><Text marginRight="20px" color="#0487D9">{getTime(date)}</Text></Button>
+                  <FormControl.HelperText>Enter time for the notification</FormControl.HelperText>
+                </FormControl>
               </HStack>
             </Actionsheet.Item>
-          }
+            <Actionsheet.Item backgroundColor={isDarkMode ? '#242525' : '#EEEEEE'}>
+              <HStack width={0.85 * Dimensions.get('window').width} justifyContent="space-between">
+                <Button backgroundColor="#0487D9" height="10" onPress={pickImage}> Select Image</Button>
+                {image && <Image resizeMode='contain' alt='Image' source={{ uri: image }} style={{ width: 108, height: 192 }} />}
+              </HStack>
+            </Actionsheet.Item>
 
-        </Actionsheet.Content>
-      </Actionsheet>
+            <Actionsheet.Item backgroundColor={isDarkMode ? '#242525' : '#EEEEEE'}>
+              <HStack width={0.85 * Dimensions.get('window').width} justifyContent="space-between"  >
+                <Button onPress={handleCancel} backgroundColor="danger.600" w="20">Cancel</Button>
+                <Button onPress={handleAdd} backgroundColor="#0487D9" w="20"><Text fontFamily="Roboto" fontSize="16px" color="white" >Add</Text></Button>
+              </HStack>
+            </Actionsheet.Item>
+            {
+              message && <Actionsheet.Item>
+                <HStack width={0.85 * Dimensions.get('window').width} justifyContent="center">
+                  <Alert status={stat} colorScheme={stat}>
+                    <HStack space={2} justifyContent="space-between" >
+                      <Alert.Icon />
+                      <Text>{message}</Text>
+                    </HStack>
+                  </Alert>
+                </HStack>
+              </Actionsheet.Item>
+            }
 
-      
-    </SafeAreaView>
+          </Actionsheet.Content>
+        </Actionsheet>
+
+
+      </SafeAreaView>
     </NativeBaseProvider>
   );
 }
--- index.js
+++ index.js
@@ -1,15 +1,125 @@
-/**
- * @format
- */
+
 
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import PushNotification from "react-native-push-notification";
+import  AsyncStorage from '@react-native-async-storage/async-storage';
+import RNFetchBlob from 'rn-fetch-blob';
+import Share from 'react-native-share';
+
+const FetchData = async () => {
+    setIsLoading(true);
+    try {
+      var stored = await AsyncStorage.getItem('data');
+      stored = JSON.parse(stored)
+      //console.log(stored.assets,1)
+      if ((stored == null) || (stored.assets == [])) {
+        console.log("No data inside local storage")//need to remove during production
+        setIsLoading(false);
+      }
+      else if (stored.assets == []) {
+        console.log("No data inside local storage but not first time")//need to remove during production
+        setIsLoading(false);
+      }
+      else {
+        console.log(stored.assets[0].key)
+      
+        setData(stored)
+        setIsLoading(false);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  const getBase64 = (key, list) => {
+    for (var i=0; i < list.length; i++){
+      if (list[i].key == key)
+        return list[i].image;
+    }
+  }
+
+  const setNotification = (key, thedate, title) => {
+
+    console.log(thedate.toLocaleString());
+    console.log(key, title);
+    console.log(typeof (thedate));
+
+    PushNotification.localNotificationSchedule({
+      id: key,
+      channelId: "wisher",
+      title: title,
+      message: "Hey mate, you got wish remainder!",
+      date: thedate,
+      allowWhileIdle: true,
+      repeatType: "year"
+    });
+    console.log("Scheduled Notification");
+    // Notifications.scheduleNotificationAsync({
+    //   identifier: key,
+    //   content: {
+    //     title: title,
+    //     body: "Hey mate, you got wish remainder!"
+    //   },
+    //   trigger: {
+    //     day: parseInt(thedate.getDate()),
+    //     month: parseInt(thedate.getMonth()),
+    //     hour: parseInt(thedate.getHours()),
+    //     minute: parseInt(thedate.getMinutes()),
+    //     repeats: true,
+    //   }
+    // })
+
+
+
+  }
 
 PushNotification.configure({
-    onNotification: function (notification) {
+    onNotification: async function (notification) {
         console.log("Notification : ",notification);
+        console.log(notification.id);
+        var date = new Date(notification.fireDate);
+        date.setFullYear(date.getFullYear()+1);
+        setNotification(notification.id, date, notification.title);
+        var stored = await AsyncStorage.getItem('data');
+        stored = JSON.parse(stored)
+        var base64 = getBase64(notification.id, stored.assets);
+        //console.log(base64);
+        await AsyncStorage.setItem("myImage", base64);
+        const imageBase = await AsyncStorage.getItem('myImage');
+        //console.log(imageBase)
+        const imagePath = `${RNFetchBlob.fs.dirs.CacheDir}/myImage.jpg`;
+        await RNFetchBlob.fs.writeFile(imagePath, imageBase, 'base64')
+        .then((success) => {
+            console.log("File Written");
+          }).catch((e) => console.log(e))
+        var imageURI = "file:///"+imagePath;
+        console.log(imageURI)
+        Share.isPackageInstalled('com.instagram.android')
+        .then(async ({ isInstalled }) => {
+        if (isInstalled){
+          await Share.shareSingle({
+            stickerImage: imageURI,
+            appId: '230235216133934',
+            social: Share.Social.INSTAGRAM_STORIES,
+            backgroundBottomColor: '#000000',
+            backgroundTopColor: '#000000',
+          })
+        } else {
+          await Share.open({ url: imageURI })
+        }
+        
+      })
+        .catch((err) => console.error(err));
+        
+        // await Share.shareSingle({
+        //     stickerImage: imageURI,
+        //     appId: '230235216133934',
+        //     social: Share.Social.INSTAGRAM_STORIES,
+        //     backgroundBottomColor: '#000000',
+        //     backgroundTopColor: '#000000',
+        // })
+        //await Share.open({ url: imageURI })
     },
     requestPermissions: Platform.OS === 'ios'
 });
